6191b56941f082405b9372f44421bf85047b3e88
Krrish <shreenidhipani2001@gmail.com>
2026-02-02 00:22:22 +0530

    adfafadad

--- ecodarshini-cms/src/collections/Users.ts
+++ ecodarshini-cms/src/collections/Users.ts
@@ -1,8 +1,26 @@
-import type { CollectionConfig } from 'payload'
+// import type { CollectionConfig } from 'payload'
+
+// export const Users: CollectionConfig = {
+//   slug: 'users',
+//   auth: true, // Enables authentication
+//   admin: {
+//     useAsTitle: 'email',
+//   },
+//   fields: [
+//     {
+//       name: 'name',
+//       type: 'text',
+//     },
+//   ],
+// }
+
+
+
+import type { CollectionConfig } from 'payload/types'
 
 export const Users: CollectionConfig = {
   slug: 'users',
-  auth: true, // Enables authentication
+  auth: true,
   admin: {
     useAsTitle: 'email',
   },
