c4dc8ab70b38ef150b02944ad320d9e13e8fd0ba
shreenidhipani2001 <shreenidhipani2001@gmail.com>
2026-02-11 23:05:51 +0530

    port changed

--- controllers/userController.js
+++ controllers/userController.js
@@ -318,9 +318,12 @@ export const getUserCredentials = async (req, res) => {
       console.log('otp'+otp);
       const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         family: 4,
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 10000,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
