985110de8f932a6be6861fa9ac2fb6a24de44ef2
shreenidhipani2001 <shreenidhipani2001@gmail.com>
2026-02-12 00:07:22 +0530

    dns solve

--- controllers/userController.js
+++ controllers/userController.js
@@ -363,9 +363,12 @@ export const sendPassword = async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       family: 4,
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
